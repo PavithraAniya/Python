294d16e89595e68da13d5570321c6e75f13c28a3
PavithraAniya <149766890+PavithraAniya@users.noreply.github.com>
2024-07-19 23:38:45 +0530

    Create Life_in_weeks.py

---
+++ Life_in_weeks.py
@@ -0,0 +1,8 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+max_age = 90*52
+current_age = int(age)*52
+weeks_left= max_age-current_age
+
+print(f"You have {weeks_left} weeks left.")
