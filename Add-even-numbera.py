449065c3a4e76ec0952f56b706b17aa54e3c0191
PavithraAniya <149766890+PavithraAniya@users.noreply.github.com>
2025-03-15 13:13:52 +0530

    Update Add-even-numbera.py

--- Add-even-numbera.py
+++ Add-even-numbera.py
@@ -1,7 +1,7 @@
 target = int(input())  # Enter a number between 0 and 1000
-# 🚨 Do not change the code above ☝️
+
 sum = 0
-# Write your code here 👇
+
 for number in range(target + 1):
     # print (number)
     # print (even_num)
@@ -9,7 +9,7 @@ for number in range(target + 1):
         sum += number
 print(sum)
 
-##Alter
+
 even_sum =0
 for no in range(0, target+1, 2):
     even_sum += no
